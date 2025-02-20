3b9d3a1ddd1c8c035200cd1d04901ef52ebdd17b
thatmagicalcat <thatmagicalcat@hotmail.com>
2025-02-20 10:17:02 +0530

    Add: schemou error type

---
+++ schemou/src/error.rs
@@ -0,0 +1,31 @@
+macro_rules! error {
+    [ $vis:vis enum $name:ident { $($variant:ident => $error_msg:expr),* $(,)? }] => {
+        #[derive(Debug)]
+        $vis enum $name {
+            $($variant),*
+        }
+
+        impl SerdeError {
+            fn description(&self) -> &'static str {
+                match self {
+                    $( Self::$variant => $error_msg, )*
+                }
+            }
+        }
+
+        impl std::error::Error for SerdeError {}
+        impl std::fmt::Display for SerdeError {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                f.write_str(self.description())
+            }
+        }
+    };
+}
+
+error! {
+    pub enum SerdeError {
+        NotEnoughData => "ran out of data bytes while parsing, cannot deserialize the remaining fields",
+        InvalidUTF8   => "raw bytes contain invalid UTF-8 data, cannot deserialize string",
+        InvalidChars  => "found invalid characters for `ShortIdStr`",
+    }
+}
